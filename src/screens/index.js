67b6f6a6a7f01467f5f6bd19471d14b9f823e2ff
Java God <95417366+TradeMarshal@users.noreply.github.com>
2023-07-20 02:53:42 +0100

    Update index.js

--- src/screens/index.js
+++ src/screens/index.js
@@ -3,3 +3,4 @@ export { default as OnboardingScreen } from './OnboardingScreen.jsx'
 export { default as LoginScreen } from './LoginScreen.jsx'
 export {default as HomeScreen } from './HomeScreen.jsx';
 export { default as SignUpScreen } from './SignUpScreen.jsx'
+export { default as Profile } from './Profile.jsx'
