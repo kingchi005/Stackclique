b862e92923aa79d79a6f75855636b79fdc3f2c70
KingDavid Chibueze <kingchi005@gmail.com>
2023-07-27 17:13:35 +0100

    added db to gitignore

--- dist/src/server.js
+++ dist/src/server.js
@@ -20,7 +20,6 @@ const swagger_ui_express_1 = __importDefault(require("swagger-ui-express"));
 const swagger_config_1 = __importDefault(require("./api-doc/swagger-config"));
 const env_1 = __importDefault(require("../env"));
 const courses_1 = __importDefault(require("./routes/courses"));
-const middleWare_1 = require("./controllers/middleWare");
 const app = (0, express_1.default)();
 const PORT = +env_1.default.PORT || 3000;
 // Middleware setup
@@ -33,7 +32,7 @@ app.get("/dev/api-docs", swagger_ui_express_1.default.setup(swagger_config_1.def
 // Routes setup
 app.use("/auth", auth_1.default);
 // secured routes
-app.use(middleWare_1.secureRoute);
+// app.use(secureRoute);
 app.use("/courses", courses_1.default);
 // Error handling middleware
 app.use((err, req, res, next) => {
