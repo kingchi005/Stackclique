d511eaed8fef99ff42c5f35193cc4a09909cf4e0
KingDavid Chibueze <kingchi005@gmail.com>
2023-07-28 13:40:35 +0100

    Susspended user phone verification

--- dist/src/api-doc/swagger-config.js
+++ dist/src/api-doc/swagger-config.js
@@ -6,5 +6,10 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const swaggerConfig_json_1 = __importDefault(require("./swaggerConfig.json"));
 const env_1 = __importDefault(require("../../env"));
 swaggerConfig_json_1.default.servers[0].url = env_1.default.BASE_URL;
+// Susspending the user sign up with phone function routes
+swaggerConfig_json_1.default.paths["/auth/get-sms-otp/{phone_numebr}"] =
+    {};
+swaggerConfig_json_1.default.paths["/auth/signup-phone"] =
+    {};
 exports.default = swaggerConfig_json_1.default;
 //# sourceMappingURL=swagger-config.js.map
--- src/api-doc/swagger-config.ts
+++ src/api-doc/swagger-config.ts
@@ -3,4 +3,12 @@ import env from "../../env";
 
 swaggerConfigJson.servers[0].url = env.BASE_URL;
 
+// Susspending the user sign up with phone function routes
+
+swaggerConfigJson.paths["/auth/get-sms-otp/{phone_numebr}"] =
+	{} as (typeof swaggerConfigJson.paths)["/auth/get-sms-otp/{phone_numebr}"];
+
+swaggerConfigJson.paths["/auth/signup-phone"] =
+	{} as (typeof swaggerConfigJson.paths)["/auth/signup-phone"];
+
 export default swaggerConfigJson;
